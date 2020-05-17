3a363d0eff3d39348a18d06d1b5b6167d63fe8b3
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-16 23:39:57 -0300

    first commit

---
+++ create_emscripten_project.sh
@@ -0,0 +1,11 @@
+#This script scaffolds an emscripten project, to save  my time
+##TODO 1) Grab the project name from command line
+##TODO 2) Create the root directory
+##TODO 2.1) Create the main file
+##TODO 2.2) Create the CMakeLists.txt
+##TODO 3) Create the build directory
+##TODO 3.1) Run the emscripten commands in the build directory
+##TODO 4) Create the deploy directory
+##TODO 4.1) Copy the docker configs to deploy directory
+##TODO 4.2) Copy the heroku configs to deploy directory
+##TODO 4.3) Set up heroku
